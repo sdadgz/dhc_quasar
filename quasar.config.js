fb592891992cac827012d9141abcbe29ca00c33c
sdadgz <pengchengma5@gmail.com>
2022-10-17 15:55:53 +0800

    非根目录部署

--- quasar.config.js
+++ quasar.config.js
@@ -60,7 +60,7 @@ module.exports = configure(function (/* ctx */) {
 
       // rebuildCache: true, // rebuilds Vite/linter/etc cache on startup
 
-      // publicPath: '/',
+      publicPath: '/web',
       // analyze: true,
       // env: {},
       // rawDefine: {}
--- src/components/Models.ts
+++ src/components/Models.ts
@@ -1,6 +1,6 @@
-export const SERVER_NAME = "http://localhost:8002";
+export const SERVER_NAME = "https://sdadgz.cn:8002";
 
 // 静态资源前缀
 export const STATIC_SRC = '/static/';
 
-export const SERVER_PREFIX = '/#'
+export const SERVER_PREFIX = '/web/#'
